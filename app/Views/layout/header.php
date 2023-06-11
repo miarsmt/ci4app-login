2771a631f77428f8765426dc22a924c62c5b4c36
miarsmt <rusmiatim973@gmail.com>
2023-06-11 15:37:40 +0700

    menambahkan parameter data

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Codeigniter 4</title>
+    <title><?= $title; ?></title>
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 
