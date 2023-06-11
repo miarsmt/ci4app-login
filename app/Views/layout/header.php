e88ef80612556df563c7c88f3a6487aaa14b269b
miarsmt <rusmiatim973@gmail.com>
2023-06-11 15:04:38 +0700

    menambahkan layouting pada view

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>About Me</title>
+    <title>Codeigniter 4</title>
     <link href="<?= base_url('assets/'); ?>css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 
