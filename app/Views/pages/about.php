6bef77528c9a0e0b970ecf40c194fa74cc5efe79
miarsmt <rusmiatim973@gmail.com>
2023-06-11 14:35:35 +0700

    menambahkan controller pages dan view home, about

---
+++ app/Views/pages/about.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Me</title>
+</head>
+
+<body>
+    <h1>About Me</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis adipisci veritatis, repellendus quas at eligendi numquam. Atque, itaque dolorum? Perspiciatis facere officia dolore eaque repellat veritatis quis recusandae earum est!</p>
+</body>
+
+</html>
