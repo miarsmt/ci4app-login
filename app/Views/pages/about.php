080bbcbe178e6435b1d59b2c10439b4cc13e7753
miarsmt <rusmiatim973@gmail.com>
2023-06-11 14:41:23 +0700

    menambahkan bootstrap ke dalam view

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,15 +1,17 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>About Me</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 
 <body>
     <h1>About Me</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis adipisci veritatis, repellendus quas at eligendi numquam. Atque, itaque dolorum? Perspiciatis facere officia dolore eaque repellat veritatis quis recusandae earum est!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae id dolorem quia modi qui facilis explicabo deserunt, doloremque, mollitia perspiciatis reprehenderit architecto, natus sint voluptates illum eum tempore nostrum vel.</p>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 
 </html>
