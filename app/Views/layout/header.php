63094ebf1d1977d57aadf176c89daf3b7dfc9550
miarsmt <rusmiatim973@gmail.com>
2023-06-11 15:10:55 +0700

    menghapus base url

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Codeigniter 4</title>
-    <link href="<?= base_url('assets/'); ?>css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 
 <body>
